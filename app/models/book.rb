383ed924fe4c4dc609122c8ef0e69f58f65dece6
TomokaAizen <147827756+aizentomoka@users.noreply.github.com>
2023-12-03 05:31:03 +0000

    Bootstrap

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
   has_many :user
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  
+   has_one_attached :profile_image
+   belongs_to :user
+   
 end
