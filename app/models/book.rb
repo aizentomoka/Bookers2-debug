8a8f9c51b046c18ac4064b94e2df87e81f2fb6cb
TomokaAizen <147827756+aizentomoka@users.noreply.github.com>
2023-12-04 09:04:03 +0000

    comment-modelの記述

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,6 +7,7 @@ class Book < ApplicationRecord
   
    belongs_to :user
    has_one_attached :profile_image
+   has_many :book_comments, dependent: :destroy
    has_many :favorites, dependent: :destroy
    
    
