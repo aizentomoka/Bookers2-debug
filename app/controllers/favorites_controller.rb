b34a00aa1897f8aad0d24a1dab5e1b54389f390b
TomokaAizen <147827756+aizentomoka@users.noreply.github.com>
2023-12-09 04:23:11 +0000

    いいねの非同期通信完了

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,8 +2,8 @@ class FavoritesController < ApplicationController
     
     
   def create
-     book = Book.find(params[:book_id])
-     favorite = current_user.favorites.new(book_id: book.id)
+     @book = Book.find(params[:book_id])
+     favorite = current_user.favorites.new(book_id: @book.id)
      favorite.save
     #  redirect_back fallback_location: book_path(book)
   end
@@ -11,8 +11,8 @@ class FavoritesController < ApplicationController
   
   
   def destroy
-     book = Book.find(params[:book_id])
-     favorite = current_user.favorites.find_by(book_id: book.id)
+     @book = Book.find(params[:book_id])
+     favorite = current_user.favorites.find_by(book_id: @book.id)
      favorite.destroy
     #  redirect_back fallback_location: book_path(book)
   end
