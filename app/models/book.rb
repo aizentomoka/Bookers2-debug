5ff7a172be84d2b66fabe1e1e4cb6731668e6b55
TomokaAizen <147827756+aizentomoka@users.noreply.github.com>
2023-12-09 15:54:22 +0000

    いいねの並び替え

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,7 +9,7 @@ class Book < ApplicationRecord
    has_one_attached :profile_image
    has_many :book_comments, dependent: :destroy
    has_many :favorites, dependent: :destroy
-   
+   has_many :favorited_users, through: :favorites, source: :user
    
    
    
